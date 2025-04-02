58c4c1370e1b84acc3a69638194c79ecce52afc4
ScytaleTleilax <andreivradulescu@gmail.comx>
2025-04-02 20:41:40 +0300

    [INIT] Implement counter functionality in Playground component

--- awesome_owl/controllers/controllers.py
+++ awesome_owl/controllers/controllers.py
@@ -2,6 +2,8 @@ from odoo import http
 from odoo.http import request, route
 
 class OwlPlayground(http.Controller):
+
+
     @http.route(['/awesome_owl'], type='http', auth='public')
     def show_playground(self):
         """
