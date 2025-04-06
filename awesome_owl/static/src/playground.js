999a2ca1c7bd031154de28d540e66e39789ed67f
ScytaleTleilax <andreivradulescu@gmail.comx>
2025-04-06 20:15:05 +0300

    [ADD] Enhance Card and Counter components; integrate state management in Playground; Coupling between Parent `playground` and Child `counter` Components

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,10 +1,21 @@
-import { Component } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
 export class Playground extends Component {
 
     static template = "awesome_owl.playground";
-    static components = { Counter , Card };
+    static components = { Counter, Card };
 
+    setup() {
+
+        this.string1 = "<div class='text-primary bg-dark'> string1 <div/>";
+        this.string2 = markup("<div class='text-primary bg-light'> string1 <div/>");
+
+        this.sum = useState({ value: 2 });
+    }
+
+    incrementSum() {
+        this.sum.value++;
+    }
 }
